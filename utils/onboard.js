d6e52ab3dd2b0829c19d12aa2a33a706c686e1d2
Danielemiccoli-dev <102368221+Danielemiccoli-dev@users.noreply.github.com>
2023-03-27 19:42:10 +0200

    Update onboard.js

--- utils/onboard.js
+++ utils/onboard.js
@@ -6,8 +6,7 @@ import fortmaticModule from '@web3-onboard/fortmatic'
 
 import Easyzoom_icon from '../easyzoomicon'
 
-const RPC_URL = 'https://sepolia.infura.io/v3/ff526ca8ef12400d997abd0bd663bb00'
-
+const rpc_url = process.env.PROVIDER_URL
 const fortmatic = fortmaticModule({
   apiKey: process.env.NEXT_PUBLIC_FORTMATIC_KEY
 })
@@ -23,25 +22,25 @@ const initOnboard = init({
     //   id: '0x1',
     //   token: 'ETH',
     //   label: 'Ethereum Mainnet',
-    //   rpcUrl: RPC_URL
+    //   rpcUrl: rpc_url
     // },
     // {
     //   id: '0x3',
     //   token: 'tROP',
     //   label: 'Ethereum Ropsten Testnet',
-    //   rpcUrl: RPC_URL
+    //   rpcUrl: rpc_url
     // },
     // {
     //   id: '0x5',
     //   token: 'gETH',
     //   label: 'Ethereum Goerli Testnet',
-    //   rpcUrl: RPC_URL
+    //   rpcUrl: rpc_url
     // }
     {
       id: '0x11155111',
       token: 'SepoliaETH',
       label: 'Sepolia Testnet',
-      rpcUrl: RPC_URL
+      rpcUrl: rpc_url
     }
   ],
   appMetadata: {
